1c1517b291eae018275ab8af47a28ec01a59dd03
Dwayne1828 <dwaynebarrameda093@gmail.com>
2025-04-09 01:30:23 +0800

    Added for loop to asks for four possible answers

--- quiz_creator_text_file.py
+++ quiz_creator_text_file.py
@@ -10,7 +10,20 @@ def create_quiz():
         while True: 
             question = input("Enter a question: ") 
             quiz_file.write(f"Question: {question}\n") 
+            
+            #Ask the user for choices 
+            for i in range(4): 
+                choice = input("Enter choices (A, B, C, D): ")
+                if i == 0: 
+                    quiz_file.write(f"Choice A: {choice}\n")
+                if i == 1: 
+                    quiz_file.write(f"Choice B: {choice}\n")
+                if i == 2:
+                    quiz_file.write(f"Choice C: {choice}\n")
+                if i == 3:
+                    quiz_file.write(f"Choice D: {choice}\n")
+            
             break
-
+        
 create_quiz()
 
