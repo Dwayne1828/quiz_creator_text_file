5c56e97b7fd2bd61519cda3630340f14cec1a8c7
Dwayne1828 <dwaynebarrameda093@gmail.com>
2025-04-09 01:20:34 +0800

    open file is now in function and loop trial, error fixed

--- quiz_creator_text_file.py
+++ quiz_creator_text_file.py
@@ -1,13 +1,16 @@
 
-#Open a text file to store the quiz questions and answers
-with open("quiz.txt", "w") as quiz_file:
-    quiz_file.write("Quiz Creator\n")
-
 #Function to asks the user for questions and answers and writes them to the file
-def create_quiz(quiz_file):
-    #While true for a loop to allow the user to decide when to stop entering questions
-    while True: 
-        question = input("Enter a question: ") 
-        quiz_file.write(f"Question: {question}\n") 
+def create_quiz():
+    #Open the file in write mode to create a new quiz file
+    with open("quiz.txt", "w") as quiz_file:
+        quiz_file.write("Quiz Creator\n")
+        quiz_file.write("\n")
 
-create_quiz(quiz_file) 
+        #While true for a loop to allow the user to decide when to stop entering questions
+        while True: 
+            question = input("Enter a question: ") 
+            quiz_file.write(f"Question: {question}\n") 
+            break
+
+create_quiz()
+
