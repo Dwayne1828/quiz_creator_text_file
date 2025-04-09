75bafb7c42b4d74be40608a53d00f30873ce7265
Dwayne1828 <dwaynebarrameda093@gmail.com>
2025-04-09 17:14:41 +0800

    funct for continuing question number on existing quizfile, error

--- quiz_creator_text_file.py
+++ quiz_creator_text_file.py
@@ -20,6 +20,19 @@ def file_maker():
         else: 
             return file_name
 
+def question_checker(file_name):
+    #Check if the file exists, if not return 0
+    if not os.path.exists(file_name): 
+        return 1
+    
+    with open(file_name, "r") as quiz_file: 
+        lines = quiz_file.readlines()
+        for line in reversed(lines):
+            if line.startswith("Question"):
+                #Get the question number from the line
+                question_number = int(lines.split()[1].strip(":"))
+                return question_number + 1 #Return the next question number
+
 #Function to asks the user for questions and answers and writes them to the file
 def create_quiz():
     #Get the file name from the user
@@ -34,11 +47,15 @@ def create_quiz():
         quiz_file.write(f"Quiz Created - {current_time_date}\n")
         quiz_file.write("\n")
 
+        #Starts the Question number at 1
+        question_number = int(question_checker(file_name))
+
         #While true for a loop to allow the user to decide when to stop entering questions
         while True: 
             question = input("Enter a question: ") 
-            quiz_file.write(f"Question: {question}\n") 
-            
+            quiz_file.write(f"Question {question_number}: {question}\n") 
+            question_number += 1
+
             #Ask the user for choices 
             for option in ["a", "b", "c", "d"]: 
                 choice = input(f"Enter choice {option}: ")
@@ -48,7 +65,7 @@ def create_quiz():
             answer = input("Enter the correct answer (a, b, c, d): ")
             quiz_file.write(f"Answer: {answer}\n")
             quiz_file.write("\n")
-            
+        
             #Ask the user if they want to add another question
             another = input("Do you want to add another question? (yes/no): ")
             if another.lower().strip() != "yes":
