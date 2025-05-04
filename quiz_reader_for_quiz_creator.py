866f43c165f71cda60e27d13d27768bc67812e76
Dwayne1828 <dwaynebarrameda093@gmail.com>
2025-05-04 23:40:26 +0800

    readlines for choices, append choice in questions dict

--- quiz_reader_for_quiz_creator.py
+++ quiz_reader_for_quiz_creator.py
@@ -1,8 +1,22 @@
 
 def quiz_reader(): 
     with open("Existingfile.txt", "r") as file:
-        lines = file.readlines()
-        print(lines)
+        lines = file.readlines() #Stores the lines in a list 
+ 
+        #Create a dictionary to store the questions, choices, and answers
+        questions = {"choices": []} #Initialize the dictionary with empty lists for choices and answer
+
+        #Iterate through the lines to check for the presence of a question, choices, and answer
+        for line in lines: 
+            line = line.strip() #Removes leading and trailing whitespace
+            
+            if "Quiz Created" in line: #Checks if the line contains the word "Quiz Created"
+                continue #If it does, skip the line
+            if "Choice" in line: #Checks if the line contains the word "Choice"
+                questions["choices"].append(line) #Adds the choices into the choices list
+
+        print(questions)
 
 quiz_reader()
+
 
