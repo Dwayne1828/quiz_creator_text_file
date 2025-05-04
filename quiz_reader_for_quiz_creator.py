27a83241b767629888091530d68c934ef7f95ddf
Dwayne1828 <dwaynebarrameda093@gmail.com>
2025-05-05 01:20:09 +0800

    list for the complete questions

--- quiz_reader_for_quiz_creator.py
+++ quiz_reader_for_quiz_creator.py
@@ -3,6 +3,7 @@ def quiz_reader():
     with open("Existingfile.txt", "r") as file:
         lines = file.readlines() #Stores the lines in a list 
  
+        complete_Questions = [] #Initialize an empty list to store the questions with answer and choices
         #Create a dictionary to store the questions, choices, and answers
         questions = {"question": "", "choices": [], "answer": None} #Initialize the dictionary with empty lists for choices and answer
 
@@ -10,7 +11,9 @@ def quiz_reader():
         for line in lines: 
             line = line.strip() #Removes leading and trailing whitespace
             
-            if "Quiz Created" in line: #Checks if the line contains the word "Quiz Created"
+            if not line:
+                continue
+            elif "Quiz Created" in line: #Checks if the line contains the word "Quiz Created"
                 continue #If it does, skip the line
             elif "Choice" in line: #Checks if the line contains the word "Choice"
                 questions["choices"].append(line) #Adds the choices into the choices list
@@ -18,8 +21,10 @@ def quiz_reader():
                 questions["answer"] = line.split(": ")[1].strip()
             else: 
                 questions["question"] += " " + line #Accumulates the question text including the Question number
-
-        print(questions["question"])
+                complete_Questions.append(questions) #Adds the question to the Questions list
+                questions = {"question": "", "choices": [], "answer": None} #Resets the dictionary for the next question
+        
+        print(complete_Questions)
 
 quiz_reader()
 
