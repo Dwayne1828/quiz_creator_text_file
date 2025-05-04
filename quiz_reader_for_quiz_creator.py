2e7f18c96d33e456eb8f77dc6e5cec117d918d8b
Dwayne1828 <dwaynebarrameda093@gmail.com>
2025-05-04 23:51:10 +0800

    stores the questions including question num in question dict

--- quiz_reader_for_quiz_creator.py
+++ quiz_reader_for_quiz_creator.py
@@ -4,7 +4,7 @@ def quiz_reader():
         lines = file.readlines() #Stores the lines in a list 
  
         #Create a dictionary to store the questions, choices, and answers
-        questions = {"choices": [], "answer": None} #Initialize the dictionary with empty lists for choices and answer
+        questions = {"question": "", "choices": [], "answer": None} #Initialize the dictionary with empty lists for choices and answer
 
         #Iterate through the lines to check for the presence of a question, choices, and answer
         for line in lines: 
@@ -16,8 +16,10 @@ def quiz_reader():
                 questions["choices"].append(line) #Adds the choices into the choices list
             elif "Answer" in line: #Checks if the line contains the word "Answer"
                 questions["answer"] = line.split(": ")[1].strip()
+            else: 
+                questions["question"] += " " + line #Accumulates the question text including the Question number
 
-        print(questions["answer"])
+        print(questions["question"])
 
 quiz_reader()
 
